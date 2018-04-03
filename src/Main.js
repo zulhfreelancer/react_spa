60e390206f5ca20a4d01ce3b27c631d277d47c86
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-04-03 06:06:12 +0000

    Add the partials and replace anchor tags with React's NavLink tags

--- src/Main.js
+++ src/Main.js
@@ -11,17 +11,21 @@ import Contact from "./Contact";
 class Main extends Component {
   render() {
     return (
+      <HashRouter>
         <div>
           <h1>Simple SPA</h1>
           <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/stuff">Stuff</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
           <div className="content">
-
+            <Route path="/" component={Home}/>
+            <Route path="/stuff" component={Stuff}/>
+            <Route path="/contact" component={Contact}/>
           </div>
         </div>
+      </HashRouter>
     );
   }
 }
