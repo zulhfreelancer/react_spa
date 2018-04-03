b938349b639defdf980214e8b60effd76e737570
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-04-03 05:59:09 +0000

    Import router and content pages (components) in main component

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,12 @@
 import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
 
 class Main extends Component {
   render() {
