ded700a1eda6ace43502914a4af3b071cedbd9bc
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-04-03 06:09:57 +0000

    Add CSS to all components

--- src/Contact.js
+++ src/Contact.js
@@ -5,8 +5,7 @@ class Contact extends Component {
     return (
       <div>
         <h2>GOT QUESTIONS?</h2>
-        <p>The easiest thing to do is post on
-        our <a href="#">forums</a>.
+        <p>Please contact me <a href="#">here</a>.
         </p>
       </div>
     );
